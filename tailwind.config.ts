f5d0a4ac9a2270f6cbaf7ed55f1565c38aae3cc0
lexscopicky <RETROGRADE1314@GMAIL.COM>
2025-08-11 15:38:54 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,6 @@
-colors: {
-  brand: {
-    50:'#e8efff',100:'#cfe0ff',200:'#a8c4ff',300:'#7fa7ff',
-    400:'#5588ff',500:'#2f6bff',600:'#0033a0',700:'#002a86',
-    800:'#001f63',900:'#0b1b4b'
-  }
-},
-borderRadius: { xl: '1rem', '2xl': '1.5rem' }
+// postcss.config.js
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
+}
